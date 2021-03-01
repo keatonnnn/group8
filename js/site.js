fe76f70038b996b052a86ab0a647d1a6fd7f567e
Keaton Brasse <nbrasse@ucsc.edu>
2021-02-28 18:36:40 -0800

    s
    s

---
+++ js/site.js
@@ -0,0 +1,103 @@
+/**
+ * Author:
+ * Created:
+ *
+ * (c) Copyright
+ **/
+
+
+//  function caFunction() {
+//   $("#spoutput").html(
+//   "<h1>CALIFORNIA</h1> <br> <p>California is the most perfect state in the world which is why it gets a pass! I love nancy pelosi!!!!!</p>"
+//
+//   );
+//  }
+// $('#CA').hover(caFunction);
+
+
+//these functions trigger the variable html within #spoutput
+
+function alFunction() {
+ $("#spoutput").html(
+   "<h1>ALABAMA</h1>");
+}
+$('#AL').hover(alFunction);
+
+
+
+
+
+function flFunction() {
+  $("#FL").style.color= "red";
+ $("#spoutput").html(
+   "<h1>FLORIDA</h1>"
+
+
+);
+}
+$('#FL').hover(flFunction);
+
+
+
+function ilFunction() {
+ $("#spoutput").html(
+   "<h1>ILLINOIS</h1>"
+
+
+);
+}
+$('#IL').hover(ilFunction);
+
+
+
+function laFunction() {
+ $("#spoutput").html(
+   "<h1>LOUISIANA</h1>"
+
+
+);
+}
+$('#LA').hover(laFunction);
+
+
+
+function msFunction() {
+ $("#spoutput").html(
+   "<h1>MISSISSIPPI</h1>"
+
+
+);
+}
+$('#MS').hover(msFunction);
+
+
+
+function ncFunction() {
+ $("#spoutput").html(
+   "<h1>NORTH CAROLINA</h1>"
+
+
+);
+}
+$('#NC').hover(ncFunction);
+
+
+
+function okFunction() {
+ $("#spoutput").html(
+   "<h1>OKLAHOMA</h1>"
+
+
+);
+}
+$('#OK').hover(okFunction);
+
+
+function txFunction() {
+ $("#spoutput").html(
+   "<h1>TEXAS</h1>"
+
+
+);
+}
+$('#TX').hover(txFunction);
